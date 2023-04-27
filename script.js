80be44da15726bd751767e6aa3c59b0809b9e0b9
Felipe-Blest <102250682+Felipe-Blest@users.noreply.github.com>
2023-04-27 00:57:15 -0400

    Intento de avance

--- script.js
+++ script.js
@@ -36,30 +36,30 @@ function obtenerDia() {
 
 obtenerDia()
 
-// let edad = () => {
-//     query = document.getElementById('fechaForm');
-//     query.addEventListener("change", function (event) {
-//         let fecha = event.target.value,
-//             date = new Date(),
-//             year = date.getFullYear() - new Date(fecha).getFullYear(),
-//             month = date.getMonth() - new Date(fecha).getMonth(),
-//             day = date.getDate() - new Date(fecha).getDate() - 1;
-//         return alert(`Su edad es: ${year} años, ${month} meses y ${day} días`)
-//     })
-// }
+let edad = () => {
+    query = document.getElementById('fechaForm');
+    query.addEventListener("change", function (event) {
+        let fecha = event.target.value,
+            date = new Date(),
+            year = date.getFullYear() - new Date(fecha).getFullYear(),
+            month = date.getMonth() - new Date(fecha).getMonth(),
+            day = date.getDate() - new Date(fecha).getDate() - 1;
+        return alert(`Su edad es: ${year} años, ${month} meses y ${day} días`)
+    })
+}
 
 // edad()
 
-// let meses = () => {
-//     query = document.getElementById('fechaForm');
-//     query.addEventListener("change", function (event) {
-//         let fecha = event.target.value,
-//             date = new Date(),
-//             year = date.getTime() - new Date(fecha).getTime(),
-//             dayfin = Math.floor(year / (1000 * 3600 * 24) - 1)
-//         return alert(`Fulanito desde su nacimiento ha vivido ${dayfin} días`)
-//     })
-// }
+let meses = () => {
+    query = document.getElementById('fechaForm');
+    query.addEventListener("change", function (event) {
+        let fecha = event.target.value,
+            date = new Date(),
+            year = date.getTime() - new Date(fecha).getTime(),
+            dayfin = Math.floor(year / (1000 * 3600 * 24) - 1)
+        return alert(`Fulanito desde su nacimiento ha vivido ${dayfin} días`)
+    })
+}
 
 // meses()
 
@@ -83,8 +83,6 @@ function cuentaRegresiva() {
     var fecha = new Date();  //la fecha de hoy
     let agno = fecha.getFullYear() + 1; //creamos una variable para el próximo año.
     let nuevoAgno = new Date(agno, 0, 1);     // de la nueva fecha tomamos el mes 0 -> enero y el día 1.
-
-
     let segundosFalta = Math.floor((nuevoAgno.getTime() - fecha.getTime()) / 1000);  //contabilizamos los milisegundo entre la fecha del año siguiente y el actual
     let dias = Math.floor((segundosFalta / (60 * 60)) / 24);  //calculamos los días dividiendo los segundos por los minutos y segundos (60*60)
     //dayElement.innerText = dias;
@@ -99,13 +97,23 @@ function cuentaRegresiva() {
 
 function horaConsulta() {
     let date = new Date(),
-        ampm = date.getHours;
-    if (ampm > 12 && ampm > 24) {
-        ampm = 'AM'
-    } else {
-        ampm = 'PM'
+        ampmR = 'AM',
+        ampm = date.getHours();
+    if (ampm >= 12 && ampm <= 24) {
+        ampmR = 'PM'
     }
-    console.log(`La hora de esta consulta fue a las: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${ampm}`)
+    console.log(`La hora de esta consulta fue a las: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${ampmR}`)
 }
 
 horaConsulta()
+
+let permanenciaOrgDias = (fechaIng, fechaActual) => {
+    let fechaIngresada = new Date(fechaIng),
+        fechaHoy = new Date(fechaActual),
+        diferencia = fechaHoy.getTime() - fechaIngresada.getTime(),
+        dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    console.log('Fulanito lleva una permanencia de', dias, 'días');
+
+}
+
+permanenciaOrgDias(2022 - 04 - 27, 2023 - 04 - 27)
